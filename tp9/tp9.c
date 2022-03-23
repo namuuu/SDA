1c772d4ae73f6b2d7b2876b36b4f639ad31c4c92
Namu <49788407+namuuu@users.noreply.github.com>
2022-03-23 10:20:34 +0100

    Update tp9.c

--- tp9/tp9.c
+++ tp9/tp9.c
@@ -2,71 +2,171 @@
 
 #include "pile.h"
 #include "file.h"
+#include "permut.h"
 
+void testPile(T_Pile *pile);
+void permut(T_Pile *pile, char *chaine);
 
-int menu()
-{
+int menu() {
 
-int choix;
-printf("\n\n\n SDA1 TP9");
-printf("\n\n\n 1 : tester mon fichier file.c");
-printf("\n 2 : tester mon fichier pile.c");
-printf("\n 3 : afficher et compter les permutations d'une chaine");
-printf("\n 4 : afficher et compter les solutions pour un échiquier ");
-printf("\n 0 :  QUITTER  ");
-printf("\n votre choix ?  ");
-scanf("%d",&choix);
-return choix;
+  int choix;
+  printf("\n\n\n SDA1 TP9");
+  printf("\n\n\n 1 : Tester mon fichier file.c");
+  printf("\n 2 : Tester mon fichier pile.c");
+  printf("\n 3 : Afficher et compter les permutations d'une chaine");
+  printf("\n 4 : Afficher et compter les solutions pour un échiquier ");
+  printf("\n\t\t 0 :  QUITTER  \n");
+  printf("\n Votre choix: ");
+  scanf("%d", & choix);
+  return choix;
 }
 
-int main()
-{
-T_File mafile;
-//T_Pile mapile;
-int chx;
-T_Elt valeur;
-// int taille;
-//char chaine[20];
+int main() {
+  T_File file;
+  T_Pile pile;
+  int chx;
+  // int taille;
+  char chaine[20];
 
-do
-{
-chx=menu();
-switch (chx)
+  do {
+    chx = menu();
+    switch (chx) {
+    case 1:
+      // Testez toutes vos fonctions par un jeu de test de votre choix
+      testPile(&pile);
+      break;
+    case 2: //testez toutes vos fonctions par un jeu de test de votre choix
+	initFile(&mafile);
+	printf("ajouter un element");
+	valeur=9;
+	filePleine(&mafile);
+	ajouter(&mafile,&valeur);
+	valeur=5;
+	ajouter(&mafile,&valeur);
+	afficherFile(&mafile);
+	retirer(&mafile,&valeur);
+	printf("\n2:\n");
+	afficherFile(&mafile);
+	printf("\n3:\n");
+	premier(&mafile);
+      break;
+    case 3:
+      scanf("%s",chaine); //une chaine de longueur <=MAX
+      permut(&pile,chaine); //TP9 partie 2: ecrire permut
+      break;
+    case 4:
+      //scanf("%d",&taille);//taille echiquier <=MAX
+      //echiquier(&mapile,taille); //TP9 partie 3: ecrire echiquier
+      break;
+
+    }
+  } while (chx != 0);
+
+  printf("\nProgramme terminé !\n");
+  return 0;
+}
+
+
+void testPile(T_Pile *pile) {
+
+	int choix;
+	T_Elt elt;
+	
+	while (choix != 0)
 	{
-	case 1 :  
-		//testez toutes vos fonctions par un jeu de test de votre choix
-		//testPile(&mapile); //TP9 partie 1 : à ecrire 
-		break;
-	case 2 : //afficherFile(&mafile); 
-			initFile(&mafile);
-			printf("ajouter un element");
-			valeur=9;
-			filePleine(&mafile);
-			ajouter(&mafile,&valeur);
-			valeur=5;
-			ajouter(&mafile,&valeur);
-			afficherFile(&mafile);
-			retirer(&mafile,&valeur);
-			printf("\n2:\n");
-			afficherFile(&mafile);
-			printf("\n3:\n");
-			premier(&mafile);
+		printf("\n\n\n Test Pile\n");
+		printf("\n 1 : initPile");
+		printf("\n 2 : pilePleine");
+		printf("\n 3 : pileVide");
+		printf("\n 4 : empiler");
+		printf("\n 5 : depiler");
+		printf("\n 6 : sommet");
+		printf("\n 7 : hauteur");
+		printf("\n 8 : afficherPile");
+		printf("\n\t\t 0 :  QUITTER  \n");
+		printf("\n Quel est votre choix ? ");
+		scanf("%d", &choix);
 
-		break; 
-	case 3 : 
-		//scanf("%s",chaine); //une chaine de longueur <=MAX
-		//permut(&mapile,chaine); //TP9 partie 2: ecrire permut
-		break;
-	case 4 : 
-		//scanf("%d",&taille);//taille echiquier <=MAX
-		//echiquier(&mapile,taille); //TP9 partie 3: ecrire echiquier
-		break;
+		switch (choix)
+		{
+		case 1:
+			initPile(pile);
+			break;
+		case 2:
+			if(pilepleine(pile)) {
+				printf("\nINFO > La pile est effectivement pleine !\n");
+			} else {
+				printf("\nINFO > La pile n'est pas pleine.");
+			}
+			getchar();
+			break;
+		case 3:
+			if(pilevide(pile)) {
+				printf("\nINFO > La pile est effectivement vide !\n");
+			} else {
+				printf("\nINFO > La pile n'est pas vide.");
+			}
+			getchar();
+			break;
+		case 4:
+			printf("Quel élément souhaitez-vous ajouter (int) : ");
+			saisirElt(&elt);
 
-
-
+			empiler(pile, elt);
+			printf("Élément empilé ! Celui-ci était : ");
+			afficherElt(&elt);
+			break;
+		case 5:
+			depiler(pile, &elt);
+			printf("Élément dépilé ! Celui-ci était : ");
+			afficherElt(&elt);
+			break;
+		case 6:
+			if(pilevide) {
+				printf("La pile est vide !");
+				break;
+			}
+			elt = sommet(pile);
+			printf("Le sommet est : ");
+			afficherElt(&elt);
+			break;
+		case 7:
+			int haut = hauteur(pile);
+			printf("La hauteur est : ");
+			afficherElt(&haut);
+			break;
+		case 8:
+			afficherPile(pile);
+			getchar();
+			break;
+		case 0:
+		default:
+			break;
+		}
 	}
-}while(chx!=0);
 
-printf("\nau plaisir de vous revoir ...\n");
-return 0;
 }
+
+void permut(T_Pile *pile, char *chaine) {
+	int taille = strlen(chaine);
+
+	while (noeudValide(pile))
+	{
+		if(1) {
+			// C'est un noeud terminal, la solution est donc correcte ! L'imprimer
+			printf("Solution : "); afficherPile(pile);
+
+			// Passer à la zone suivante à vérifier
+			while (1)
+			{
+				
+			}
+			
+		} else {
+
+		}
+	}
+	
+
+	*pile; *chaine;
+}	
